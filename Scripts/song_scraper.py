04ad0d08d2dc5dbb6805aec8d90c790331289c6b
Emily Ling <eling8@stanford.edu>
2015-11-25 22:08:03 -0800

    Got all song titles.

--- Scripts/song_scraper.py
+++ Scripts/song_scraper.py
@@ -7,63 +7,58 @@ import time
 import random
 import os
 import shutil
+import sys
+
+if len(sys.argv) <= 1:
+	print "Must include file name as argument."
+	exit(1)
 
 baseURL = "http://www.azlyrics.com/"
+
 # Same comment as all the other scripts. This gets the root path of our repo
 # because data files and dirs are not in the same place anymore; this is safer.
 REPO_ROOT = os.popen("git rev-parse --show-toplevel").read().strip('\n')
 folderName = REPO_ROOT + '/Data/songs'
-maxCount = 200
-# songCount = 0
-# artistCount = 0
-
-START = 11 # start at this file
-END = 15
-NUM_ARTISTS = 158 # max number
 
 # make folder for song text files
 if not os.path.exists(folderName):
 	os.mkdir(folderName)
 
-for i in range(START, min(NUM_ARTISTS + 1, END)):
-	artists = open(os.path.join('artists', 'artists' + str(i) + '.txt'), 'r')
+fileName = sys.argv[1]
+artists = open(fileName, 'r')
 
-	textPath = os.path.join(folderName, 'songs' + str(i) + '.txt')
+# after random number of songs, wait a longer period of time
+counter = random.uniform(10, 15)
+
+# for each artist, gather list of songs
+for line in artists:
+	artistName = line.split(' <//> ')[0].title()
+	artistURL = line.split(' <//> ')[1]
+
+	textPath = os.path.join(folderName, artistName + '.txt')
 	if os.path.exists(textPath):
 		os.remove(textPath)
-
 	songs = open(textPath, 'a')
 
-	# for each artist, gather list of songs
-	for line in artists:
-		artistName = line.split(' <//> ')[0].title()
-		temp = artistName
-		if ',' in artistName and not artistName == "Chunk! No, Captain Chunk!":
-			artistName = artistName.split(', ')[1] + " " + artistName.split(', ')[0]
-			# print temp, "||", artistName
-		artistURL = line.split(' <//> ')[1]
+	page = urllib2.urlopen(baseURL + artistURL)
+	soup = BeautifulSoup(page, 'html.parser')
 
-		page = urllib2.urlopen(baseURL + artistURL)
-		soup = BeautifulSoup(page, 'html.parser')
+	for link in soup.find_all('a', { 'target' : "_blank" }):
+		if not link.get_text() == None and '../lyrics' in link.get('href'):
+			songs.write(artistName + " <//> " + link.get_text().encode('utf8') + " <//> " + link.get('href').encode('utf8') + '\n')
+			
+	songs.close()
 
-		for link in soup.find_all('a', { 'target' : "_blank" }):
-			if not link.get_text() == None and '../lyrics' in link.get('href'):
-				# if not songCount / maxCount == (songCount - 1) / maxCount:
-				# 	songs = open(folderName + '/songs' + str(songCount / maxCount) + '.txt', 'a')
+	pause = random.uniform(7, 13)
+	print "Finished", artistName, '|| pausing for', pause
+	time.sleep(pause)
 
-				songs.write(artistName + " <//> " + link.get_text().encode('utf8') + " <//> " + link.get('href').encode('utf8') + '\n')
-				
-				# if not songCount / maxCount == (songCount + 1) / maxCount:
-				# 	songs.close()
-				# songCount += 1
-
-				# print link.get_text(), link.get('href')
-
-		pause = random.uniform(5, 9)
-		print "Finished", artistName, '|| pausing for', pause
+	counter -= 1
+	if counter <= 0:
+		pause = random.uniform(60, 90)
+		print "Long pause: ", pause
 		time.sleep(pause)
 
-	songs.close()
-	print "Done with file", textPath
+		counter = random.uniform(10, 15)
 
 print 'Finished scraping!'
